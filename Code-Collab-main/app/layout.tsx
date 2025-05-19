3c20c7314db4dd852aa56d3b9115c0fcbad0f934
vishalraj096 <vishal20031singh@gmail.com>
2025-05-19 23:26:07 +0530

    fix title bar

--- Code-Collab-main/app/layout.tsx
+++ Code-Collab-main/app/layout.tsx
@@ -52,6 +52,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg"/>
+        <title>Code Collab - Real-Time Collaborative Coding</title>
+      </head>
       <RecoilRoot>
         <body className={inter.className}>
           {/* <Navbar /> */}
